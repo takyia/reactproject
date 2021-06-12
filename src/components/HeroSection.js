7c1828d77d99716528e099634f0d9bf1d39ff190
Takyia <takyiahall@yahoo.com>
2021-06-12 13:30:31 -0400

    added cards to homepage

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,7 +1,8 @@
 import React from "react";
-import '../App.css';
-import { Button } from './Button';
-import './HeroSection.css';
+import "../App.css";
+import { Button } from "./Button";
+import "./HeroSection.css";
+import Cards from "./Cards";
 
 function HeroSection() {
   return (
@@ -9,7 +10,7 @@ function HeroSection() {
       <video src="/videos/video-1.mp4" autoPlay loop muted />
       <h1>VACA ADVENTURES</h1>
       <p>Escape and breathe the air of new places.</p>
-      {/* <img src="./images/img-home.jpg" alt="" /> */}
+      {/* <img src="/images/img-home.jpg" alt="" /> */}
       <div className="hero-btns">
         <Button
           className="btns"
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -25,7 +25,7 @@ function Navbar() {
       <nav className="navbar">
         <div className="navbar-container">
           <Link to="/" className="navbar-logo">
-            TRVL <i className="fab fa-typo3"></i>
+            VACA-SPOT <i className="fab fa-typo3"></i>
           </Link>
           <div className="menu-icon" onClick={handleClick}>
             <i className={click ? "fas fa-times" : "fas fa-bars"} />
